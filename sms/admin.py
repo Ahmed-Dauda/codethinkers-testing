c30992a3fd3c93f31a8b664d66106d3361c1097a
Ahmed-Dauda <addatainsight@gmil.com>
2021-11-22 00:42:23 -0800

    ok3

--- sms/admin.py
+++ sms/admin.py
@@ -2,26 +2,27 @@ from django.contrib import admin
 from sms.models import Categoriess, Coursess, Topicss, Comments
 # Register your models here.
 
-# @admin.register(Comments)
-# class commentadmin(admin.ModelAdmin):
-#     # list_display = ['id', 'name', 'desc', 'created']
-#     # list_filter = ordering = ['created']
-#     ordering = []
+@admin.register(Comments)
+class commentadmin(admin.ModelAdmin):
+    # list_display = ['id', 'name', 'desc', 'created']
+    # list_filter = ordering = ['created']
+    ordering = ['created']
 
-# @admin.register(Categoriess)
-# class categoriesadmin(admin.ModelAdmin):
-#     # list_display = ['id', 'name', 'desc', 'created']
-#     ordering = []
+@admin.register(Categoriess)
+class categoriesadmin(admin.ModelAdmin):
+    # list_display = ['id', 'name', 'desc', 'created']
+    ordering = ['created']
 
-# @admin.register(Coursess)
-# class coursesadmin(admin.ModelAdmin):
-# #     list_display = ['id', 'name', 'desc', 'created']
-#     ordering = []
+@admin.register(Coursess)
+class coursesadmin(admin.ModelAdmin):
+#     list_display = ['id', 'name', 'desc', 'created']
+    ordering = ['created']
 
-# @admin.register(Topicss)
-# class topicsadmin(admin.ModelAdmin):
-# #     list_display = ['id', 'name', 'desc', 'created']
-#     ordering = []
+@admin.register(Topicss)
+class topicsadmin(admin.ModelAdmin):
+#     list_display = ['id', 'name', 'desc', 'created']
+    ordering = ['created']
+
 
 
 
