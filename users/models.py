63485ee604eaa99ff25b8cd83896e499210bc0e5
Ahmed-Dauda <addatainsight@gmil.com>
2021-11-22 08:35:12 -0800

    ok33

--- users/models.py
+++ users/models.py
@@ -44,9 +44,9 @@ country_choice = [
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=254, unique=True)
-    first_name = models.CharField(max_length=254, null=True, blank=True)
-    last_name = models.CharField(max_length=254, null=True, blank=True)
-    country = models.CharField(choices = country_choice, max_length=254, null=True, blank=True)
+    # first_name = models.CharField(max_length=254, null=True, blank=True)
+    # last_name = models.CharField(max_length=254, null=True, blank=True)
+    # country = models.CharField(choices = country_choice, max_length=254, null=True, blank=True)
     # is_staff = models.BooleanField(default=False)
     # is_superuser = models.BooleanField(default=False)
     # is_active = models.BooleanField(default=True)
