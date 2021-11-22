6b4437b7804799b22caf902c500e23e4301b6d5f
Ahmed-Dauda <addatainsight@gmil.com>
2021-11-22 09:06:08 -0800

    ok33

--- users/admin.py
+++ users/admin.py
@@ -1,39 +1,39 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# # Register your models here.
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# from .models import User
+from users.models import User
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
-# class UserAdmin(BaseUserAdmin):
-#     fieldsets = (
-#         (None, {'fields': ('email', 'password','country', 'last_login')}),
-#         ('Permissions', {'fields': (
-#             'is_active', 
-#             'is_staff', 
-#             'is_superuser',
-#             'groups', 
-#             'user_permissions',
-#         )}),
-#     )
-#     add_fieldsets = (
-#         (
-#             None,
-#             {
-#                 'classes': ('wide',),
-#                 'fields': ('first_name','first_name','email','country', 'password1', 'password2')
-#             }
-#         ),
-#     )
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password','country', 'last_login')}),
+        ('Permissions', {'fields': (
+            'is_active', 
+            'is_staff', 
+            'is_superuser',
+            'groups', 
+            'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('first_name','first_name','email','country', 'password1', 'password2')
+            }
+        ),
+    )
 
-#     list_display = ('first_name','first_name', 'email','country', 'is_staff', 'last_login')
-#     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-#     search_fields = ('email',)
-#     ordering = ('email',)
-#     filter_horizontal = ('groups', 'user_permissions',)
+    list_display = ('first_name','first_name', 'email','country', 'is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
 
-# admin.site.register(User, UserAdmin)
+admin.site.register(User, UserAdmin)
