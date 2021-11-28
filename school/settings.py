9788fddf44a985af924fa1f52965280012442180
Ahmed-Dauda <addatainsight@gmil.com>
2021-11-28 14:03:21 -0800

    dsdf

--- school/settings.py
+++ school/settings.py
@@ -174,7 +174,7 @@ else:
 # DEFAULT_FROM_EMAIL = 'CodingWithMitch Team <noreply@codingwithmitch.com>'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 # EMAIL_HOST_USER = 'securesally@gmail.com'
 EMAIL_HOST_USER = 'thinkerscode@gmail.com'
 EMAIL_HOST_PASSWORD = 'olfxieyveruumqrx'
