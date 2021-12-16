e02a4dbbbb8aeb21b93a942a96be57509e26013d
Ahmed-Dauda <addatainsight@gmil.com>
2021-12-16 08:40:33 -0800

    Added files and changes required for deployment to heroku

--- users/forms.py
+++ users/forms.py
@@ -33,18 +33,31 @@ from allauth.account.forms import SignupForm
 from django import forms
 from .models import *
 country_choice = [
-    ('select country here', 'select country here'),('Nigeria', 'Nigeria'), ('United State', 'United State'), ('Nigeria', 'Nigeria')
+    ('select country here', 'select country here'),('Nigeria', 'Nigeria'), ('United State', 'United State'), ('Afghanistan', 'Afghanistan'),
+    ('Albania', 'Albania'),('Algeria', 'Algeria'), ('Andorra', 'Andorra'), ('Angola', 'Angola'),
+    ('Antigua and Barbuda', 'Antigua and Barbuda'),('Argentina', 'Argentina'), ('Armenia', 'Armenia'), ('Australia', 'Australia'),
+    ('Austria', 'Austria'),('Azerbaijan', 'Azerbaijan'), ('Bahamas', 'Bahamas'), ('	Bahrain', '	Bahrain'),
+    ('Bahamas', 'Bahamas'),('Bahrain', 'Bahrain'), ('Bangladesh', 'Bangladesh'), ('Barbados', 'Barbados'),
+    ('Belarus', 'Belarus'),('	Belgium', '	Belgium'), ('Belize', 'Belize'), ('Benin', 'Benin'),
+    ('Bhutan', 'Bhutan'),('Bolivia', 'Bolivia'), ('Bosnia and Herzegovina', 'Bosnia and Herzegovina'), ('Botswana', 'Botswana'),
+    ('Brazil', 'Brazil'),('Brunei', 'Brunei'), ('Bulgaria', 'Bulgaria'), ('Burkina Faso', 'Burkina Faso'),
+    ('Burundi', 'Burundi'),('Côte d"Ivoire', 'Côte d"Ivoire'), ('Cabo Verde', '	Cabo Verde'), ('Cambodia', 'Cambodia'),
+    ('Cameroon', 'Cameroon'),('Canada', 'Canada'), ('Central African Republic', 'Central African Republic'), ('Chad', 'Chad'),
+    ('Chile', 'Chile'),('China', 'China'), ('Colombia', 'Colombia'), ('Comoros', 'Comoros', ('Congo (Congo-Brazzaville)'),('Congo (Congo-Brazzaville)')),
+    ('Costa Rica', 'Costa Rica'),('Croatia', 'Croatia'), ('Cuba', 'Cuba'), ('Cyprus', 'Cyprus'),('Dominican Republic','Dominican Republic'),
+    ('Czechia (Czech Republic)', 'Democratic Republic of the Congo'),('Denmark', 'Denmark'), ('Djibouti', 'Djibouti'), ('Dominica', 'Dominica'),
+
 ]
 
 class SimpleSignupForm(SignupForm):
     first_name = forms.CharField(max_length=12, label='First-name')
     last_name  = forms.CharField(max_length=225, label='Last-name')
-    phone_number = forms.CharField(max_length=12, label='Phone-number')
+    # phone_number = forms.CharField(max_length=12, label='Phone-number')
     countries = forms.ChoiceField(choices = country_choice, label='Country')
     
     def save(self, request):
         user = super(SimpleSignupForm, self).save(request)
-        user.phone_number = self.cleaned_data['phone_number']
+        # user.phone_number = self.cleaned_data['phone_number']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.countries = self.cleaned_data['countries']
