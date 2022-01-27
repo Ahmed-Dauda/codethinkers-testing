be40a6ae867b9f81e7e1857a7eb86f64ccf2e6a0
Ahmed-Dauda <addatainsight@gmil.com>
2022-01-27 00:56:29 -0800

    dsdfs

--- school/urls.py
+++ school/urls.py
@@ -20,13 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 # from django.contrib.auth import views as auth_views #import this
 
-urlpatterns = [
-    
+urlpatterns = [   
     path('admin/', admin.site.urls),
     path('', include('sms.urls')),
     path('student/', include('student.urls')),
     path('accounts/', include('allauth.urls')),
-  
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
