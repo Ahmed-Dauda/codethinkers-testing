3dc7391eb1d6c58c559d068acdb7cef1defc1605
Ahmed-Dauda <addatainsight@gmil.com>
2022-07-20 20:14:26 +0100

    Added files and changes required for deployment to heroku

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,9 @@
+import imp
 from django.contrib import admin
-
+from student.models import Logo
 # Register your models here.
+class LogoAdmin(admin.ModelAdmin):
+    list_display = ("logo",)
+
+admin.site.register(Logo,  LogoAdmin)
+
