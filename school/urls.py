4b295f1f6bcb5e7bee67adc7b218405be5615b87
Ahmed-Dauda <addatainsight@gmil.com>
2022-07-25 10:01:06 +0100

    Added files and changes required for deployment to heroku

--- school/urls.py
+++ school/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('teacher/', include('teacher.urls')),
     path('accounts/', include('allauth.urls')),
     path('tinymce/', include('tinymce.urls')),
+ 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
