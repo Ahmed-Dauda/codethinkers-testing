a56b54d6f960a70c02c3ce1d9da4fc13691045f9
Ahmed-Dauda <addatainsight@gmil.com>
2022-08-04 15:30:39 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 # from django.contrib.auth import get_user_model
-# User = get_user_model()
+User = get_user_model()
 
 from django.db.models.signals import post_save, pre_save
 
