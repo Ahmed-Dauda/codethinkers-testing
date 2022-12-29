f1e8bb9c821bb6e5496a8a5f295eb84791503911
Ahmed-Dauda <addatainsight@gmail.com>
2022-12-29 16:00:44 +0100

    first commit

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,6 @@
 import imp
 from django.contrib import admin
-from student.models import Logo, signature
+from student.models import Logo, signature, Designcert
 # Register your models here.
 class LogoAdmin(admin.ModelAdmin):
     list_display = ("logo",)
@@ -11,3 +11,8 @@ class SignatureAdmin(admin.ModelAdmin):
     list_display = ("sign",)
 
 admin.site.register(signature,  SignatureAdmin)
+
+class DesigncertAdmin(admin.ModelAdmin):
+    list_display = ("design",)
+
+admin.site.register(Designcert,  DesigncertAdmin)
