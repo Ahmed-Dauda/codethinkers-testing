6854c1674a584c641e24fe4283825401ddbf8c5d
Ahmed-Dauda <addatainsight@gmail.com>
2023-03-19 14:09:31 -0700

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- sms/static/sms/dashboard/js/demo/datatables-demo.js
+++ sms/static/sms/dashboard/js/demo/datatables-demo.js
@@ -1,4 +1,11 @@
 // Call the dataTables jQuery plugin
+// $(document).ready(function() {
+//   $('#dataTable').DataTable();
+// });
+
 $(document).ready(function() {
-  $('#dataTable').DataTable();
-});
+  $('#dataTable').dataTable( {
+      "lengthMenu": [[1, 2, 3, -1], [1, 2, 3, "All"]],
+    
+  } );
+} )
