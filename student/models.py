0e3d2512e3430b24b9fa736db4cb5c0b2011cd08
Ahmed-Dauda <addatainsight@gmail.com>
2023-04-14 04:28:31 -0700

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- student/models.py
+++ student/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # from users.models import NewUser
 # from django.conf import settings
-
+from cloudinary.models import CloudinaryField
+from tinymce.models import HTMLField
+from tinymce import models as tinymce_models
 
 class Logo(models.Model):
    
@@ -11,6 +13,13 @@ class Logo(models.Model):
    def __str__(self):
         return f"{self.logo}"
 
+class PartLogo(models.Model):
+   
+   logo = CloudinaryField('image', blank=True, null= True)
+   
+   def __str__(self):
+        return f"{self.logo}"
+   
 class signature(models.Model):
        
    sign = CloudinaryField('image', blank=True, null= True)
@@ -24,3 +33,4 @@ class Designcert(models.Model):
    
    def __str__(self):
         return f"{self.design}"  
+
