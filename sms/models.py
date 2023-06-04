7a2e5194d2999bcd77ca5773247fe90320edd53c
Ahmed-Dauda <addatainsight@gmail.com>
2023-06-04 13:23:31 -0700

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- sms/models.py
+++ sms/models.py
@@ -166,13 +166,25 @@ class Blogcomment(models.Model):
 class Alert(models.Model):
 
     title = models.CharField(max_length=100, null=True)
-    content = models.TextField()
-    price = models.DecimalField (max_digits=10, decimal_places=2, default= '10000' ,max_length=225, blank=True, null= True)
+    content = models.TextField(blank=True, null= True)
+    img_ebook = CloudinaryField('Ebook images', blank=True, null= True)
+    price = models.DecimalField (max_digits=10, decimal_places=2, default= '1500' ,max_length=225, blank=True, null= True)
     created = models.DateTimeField(auto_now_add=True,blank=True, null= True)
  
     def __str__(self):
         return f'{self.title}'
     
+class Partners(models.Model):
+
+    title = models.CharField(max_length=100, null=True)
+    # content = models.TextField(blank=True, null= True)
+    img_partner = CloudinaryField('partner images', blank=True, null= True)
+    # price = models.DecimalField (max_digits=10, decimal_places=2, default= '1500' ,max_length=225, blank=True, null= True)
+    # created = models.DateTimeField(auto_now_add=True,blank=True, null= True)
+ 
+    def __str__(self):
+        return f'{self.title}'
+       
 class Gallery(models.Model):
 
     title = models.CharField(max_length=100, null=True)
