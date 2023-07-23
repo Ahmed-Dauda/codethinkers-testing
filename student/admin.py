0e6dc2fd065852c093fd582cb13717bf859efc76
Ahmed-Dauda <addatainsight@gmail.com>
2023-07-23 10:10:37 -0700

    ss

--- student/admin.py
+++ student/admin.py
@@ -1,6 +1,6 @@
 import imp
 from django.contrib import admin
-from student.models import Logo, signature, Designcert, PartLogo, Payment
+from student.models import Logo, signature, Designcert, PartLogo, PaymentN
 # from student.models import Cart,CartItem, Order, OrderItem
 
 from django.contrib import admin
@@ -11,7 +11,7 @@ from django.contrib import admin
 # admin.site.register(Order)
 # admin.site.register(OrderItem)
 
-admin.site.register(Payment)
+admin.site.register(PaymentN)
 
 # Register your models here.
 class LogoAdmin(admin.ModelAdmin):
