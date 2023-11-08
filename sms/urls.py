68002b3ed2097e46891402ea5830b56b68fe3932
Ahmed-Dauda <addatainsight@gmail.com>
2023-11-08 13:17:39 -0800

    Added files and changes required for deployment

--- sms/urls.py
+++ sms/urls.py
@@ -81,7 +81,8 @@ urlpatterns = [
     
 
     # path('certificates/<pk>/', views.Certificates, name ='certificates'),
-    path('certificates/<pk>/', Certdetaillistview.as_view(), name ='certificates'),
+    path('certificates/<int:pk>/', Certdetaillistview.as_view(), name='certificates'),
+
     path('bloglistview', Bloglistview.as_view(), name ='bloglistview'),
     path('blog/<slug:slug>/', Blogdetaillistview.as_view(), name='blogdetaillistview'),
     
