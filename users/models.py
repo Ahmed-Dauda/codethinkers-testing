95005a6638385076e9fa281c68fd158f518d476f
Ahmed-Dauda <addatainsight@gmail.com>
2023-12-02 11:47:17 -0800

    First version of application moved into GitHub

--- users/models.py
+++ users/models.py
@@ -177,13 +177,13 @@ from django.db import models
 # NewUser = get_user_model()
 
 class Referrer(models.Model):
-    user = models.OneToOneField(NewUser, on_delete=models.CASCADE)
+    learner = models.OneToOneField(NewUser, on_delete=models.CASCADE)
     referrer_code = models.CharField(max_length=20, blank=True, null=True)
     referrer = models.ForeignKey(NewUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='referred_users')
     referred_students = models.ManyToManyField(NewUser, related_name='referrer_profiles', blank=True)
 
     def __str__(self):
-        return f'Referrer Profile for {self.user.username}'
+        return f'Referrer Profile for {self.learner}'
 
     # def referred_students_count(self):
     #     return self.referred_students.count()
