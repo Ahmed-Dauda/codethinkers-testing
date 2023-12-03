8ea313036d237558fb0de80e1b8e0608cd85fba1
Ahmed-Dauda <addatainsight@gmail.com>
2023-12-02 16:37:22 -0800

    First version of application moved into GitHubi

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models heres.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import Profile, NewUser
@@ -29,7 +29,7 @@ class NewUserResource(resources.ModelResource):
         # fields = ('title',)
                
 class NewUserAdmin(ImportExportModelAdmin):
-    list_display = ['id', 'email','username','code','phone_number', 'first_name', 'last_name','countries' ,'is_staff', 'is_superuser', 'is_active','last_login', 'date_joined']
+    list_display = ['id', 'email','username','phone_number', 'first_name', 'last_name','countries' ,'is_staff', 'is_superuser', 'is_active','last_login', 'date_joined']
     list_filter =  ['id', 'email','username','first_name', 'last_login']
     search_fields = ['id', 'email','username', 'first_name', 'last_login']
     ordering = ['id']
