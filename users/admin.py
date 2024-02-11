1d96e5c51fe5bcf21975c200c446e4a0b08d30e4
Ahmed-Dauda <addatainsight@gmail.com>
2024-02-11 11:36:27 -0800

    Fixed migrate error

--- users/admin.py
+++ users/admin.py
@@ -31,8 +31,8 @@ class NewUserResource(resources.ModelResource):
 class NewUserAdmin(ImportExportModelAdmin):
     list_display = ['id', 'email','username','phone_number', 'first_name', 'last_name','student_class','school','countries' ,'is_staff', 'is_superuser', 'is_active','last_login', 'date_joined']
     list_filter =  ['id', 'email','username','phone_number','first_name', 'last_login']
-    search_fields = ['id', 'email','username','phone_number','student_class','first_name', 'last_login']
-    ordering = ['id']
+    search_fields = ['email','username','phone_number','student_class','first_name', 'last_login']
+    ordering = ['date_joined']
     
     resource_class = NewUserResource
 
@@ -53,8 +53,8 @@ class ProfileResource(resources.ModelResource):
 class ProfileAdmin(ImportExportModelAdmin):
     list_display = ['id', 'user','username', 'first_name', 'last_name','gender', 'phone_number', 'countries','pro_img', 'bio', 'created','updated']
     list_filter =  ['id', 'user','username', 'first_name', 'last_name','gender' ]
-    search_fields = ['id', 'user__email','user__first_name','user__last_name', 'username', 'gender']
-    ordering = ['id']
+    search_fields = ['user__email','user__first_name','user__last_name', 'username', 'gender']
+    ordering = ['created']
     
     resource_class = ProfileResource
 
