5978626a7fa377ab42a8f76d0a550ff8cf6679c5
Ahmed-Dauda <addatainsight@gmail.com>
2025-08-21 01:22:28 +0100

    Remove accidentally added temp-clean directory

--- sms/models.py
+++ sms/models.py
@@ -156,7 +156,7 @@ class Whatyouwilllearn(models.Model):
     
 class Whatyouwillbuild(models.Model):
     
-    desc = models.CharField(max_length=900,null=True, blank =True )
+    desc = models.TextField(max_length=900,null=True, blank =True )
     courses = models.ForeignKey(Courses, on_delete= models.CASCADE, null= True) 
     created = models.DateTimeField(auto_now_add=True,blank=True, null= True)
     updated = models.DateTimeField(auto_now=True, blank=True, null= True)
