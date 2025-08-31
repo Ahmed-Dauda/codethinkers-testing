2e4d22cd2f161598d0a4076d37a941d0c861af30
Ahmed-Dauda <addatainsight@gmail.com>
2025-08-31 01:23:34 +0100

    for Heroku build

--- webprojects/urls.py
+++ webprojects/urls.py
@@ -3,8 +3,14 @@ from webprojects.views import ai_python_completion,ai_suggest_code, auto_save_vi
 
 urlpatterns = [
 path('project/<int:project_id>/file/<int:file_id>/delete/', file_delete, name='file_delete'),
- path('projects/<int:project_id>/file/<int:file_id>/preview/', file_preview, name='file_preview'),
+path('projects/<int:project_id>/file/<int:file_id>/preview/', file_preview, name='file_preview'),
 path('<int:project_id>/upload-file-ajax/', upload_file_ajax, name='upload_file_ajax'),
+
+
+    path('<int:project_id>/create-file/', create_file, name='create_file'),
+    path('<int:project_id>/create-folder/', create_folder, name='create_folder'),
+    path('<int:project_id>/', project_detail, name='project_detail'),
+    path('<int:project_id>/file/<int:file_id>/', file_detail, name='file_detail'),
 
     path("autosave/", auto_save_view, name="file_autosave"),
 
@@ -20,9 +26,4 @@ path('<int:project_id>/upload-file-ajax/', upload_file_ajax, name='upload_file_a
     path("run-python/", run_python_code, name="run_python_code"),
     path('explain-code/', explain_code_view, name='explain_code'),
     path('ai-suggest/', ai_suggest_code, name='ai_suggest_code'),
-
-    path('<int:project_id>/create-file/', create_file, name='create_file'),
-    path('<int:project_id>/create-folder/', create_folder, name='create_folder'),
-    path('<int:project_id>/', project_detail, name='project_detail'),
-    path('<int:project_id>/file/<int:file_id>/', file_detail, name='file_detail'),
 ]
