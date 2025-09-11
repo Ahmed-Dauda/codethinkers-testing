ccc27a37728c1265eaf779b938765b53e6bb063b
Ahmed-Dauda <addatainsight@gmail.com>
2025-09-11 10:49:38 +0100

    for Heroku build

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/fastapi-hello")
 def read_root():
-    return {"message": "Hello World"}
+    return {"message": "Hello from FastAPI!"}
