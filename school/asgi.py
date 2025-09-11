4348cfb1ac8a8682da624e1cac8bcd360de12778
Ahmed-Dauda <addatainsight@gmail.com>
2025-09-11 13:52:16 +0100

    Added FastAPI + Django ASGI support

--- school/asgi.py
+++ school/asgi.py
@@ -7,10 +7,39 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
+
+# application = get_asgi_application()
+
+
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from fastapi import FastAPI
+from django.urls import path
+from django.http import JsonResponse
+from starlette.middleware.wsgi import WSGIMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "school.settings")
+django.setup()
 
-application = get_asgi_application()
+# Django ASGI app
+django_app = get_asgi_application()
+
+# FastAPI app
+fastapi_app = FastAPI()
+
+@fastapi_app.get("/api/hello")
+async def hello(name: str = "World"):
+    return {"message": f"Hello, {name}!"}
+
+# Combine Django + FastAPI
+from starlette.applications import Starlette
+
+application = Starlette()
+application.mount("/", WSGIMiddleware(django_app))       # Django handles normal routes
+application.mount("/fastapi", fastapi_app)               # FastAPI under /fastapi/*
