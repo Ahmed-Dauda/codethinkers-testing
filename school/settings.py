d0736a0a3abb9c30902b35001840caec77b1a6be
Ahmed-Dauda <addatainsight@gmail.com>
2025-09-11 19:27:55 +0100

    Fix FastAPI mount under /fastapi

--- school/settings.py
+++ school/settings.py
@@ -474,7 +474,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 import dj_database_url
 # Update database configuration from $DATABASE_URL.
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=0)
 DATABASES['default'].update(db_from_env)
 
 
