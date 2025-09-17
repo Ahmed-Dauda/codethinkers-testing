372695ffd0bf597033ce1a467ac75ba3a5e530fc
Ahmed-Dauda <addatainsight@gmail.com>
2025-09-17 10:51:27 +0100

    Add uvicorn to requirements and fix Procfile

--- django_fastapi/models.py
+++ django_fastapi/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 class ProgramPage(models.Model):
     title = models.CharField(max_length=255)
-    subtitle = models.CharField(max_length=255)
+    # subtitle = models.CharField(max_length=255)
     description = models.TextField()
-    cta_text = models.CharField(max_length=50)
+    # cta_text = models.CharField(max_length=50)
     benefits = models.TextField()  # List of benefits
 
     def __str__(self):
         return self.title
+
+    def get_benefits_list(self):
+        return self.benefits.splitlines()
