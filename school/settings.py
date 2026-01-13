00dfc893473361623406024af5c4a6826f7c1daf
Ahmed-Dauda <addatainsight@gmail.com>
2026-01-13 11:50:45 +0100

    ddd

--- school/settings.py
+++ school/settings.py
@@ -458,16 +458,28 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # # Update database configuration from $DATABASE_URL
 
+#production settings for heroku
+
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=0)
-DATABASES['default'].update(db_from_env)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=0,
+        ssl_require=False
+    )
 }
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=0)
+# DATABASES['default'].update(db_from_env)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
